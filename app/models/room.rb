69995ce7bf4637acab2234daefbf4b5e0ff93470
Maxime Plasse <m20plasse@gmail.com>
2022-03-24 14:59:08 +0100

    set up + models creation

---
+++ app/models/room.rb
@@ -0,0 +1,8 @@
+class Room < ApplicationRecord
+  belongs_to :hotel
+  has_many :bookings, dependent: :destroy
+  validates :capacity, presence: true
+  validates :capacity, numericality: { only_integer: true }
+  validates :price_per_night, presence: true
+  validates :price_per_night, numericality: { greater_than: 0 }
+end
