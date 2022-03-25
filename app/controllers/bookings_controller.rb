706f18a83029c57f93ae11987f496adf64ec3ca3
Maxime Plasse <m20plasse@gmail.com>
2022-03-25 12:15:23 +0100

    last design

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,9 +7,9 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.save
     if @booking.save
-      redirect_to room_path(@room), notice: "Demande de réservation envoyée !"
+      redirect_to room_path(@room), notice: "Demande de réservation envoyée ! Suivez votre demande sur votre espace profil"
     else
-      redirect_to room_path(@room), notice: "Votre réservation n'a pas été prise en compte !"
+      redirect_to room_path(@room), notice: "Veuillez renseigner une date d'arrivée et de départ."
     end
   end
 
